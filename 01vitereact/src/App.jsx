0902433949c85aeac2dc0d9ec39fed57d504bf24
tdk-bwcb <ygf2024@gmail.com>
2024-12-22 11:26:57 +0530

    react flow

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,19 @@
-
+import Chai from "./Chai"
 
 
 function App() {
   
 
   return (
-    <h1>Chai aur React | Vite</h1>
+    // <h1>Chai aur React | Vite</h1>
+    // <Chai/>
+    // can return only 1 element
+    // therefore enclose in a div / fragment
+    <>
+      <Chai/>
+      <p>This is a para</p>
+      <h4>this is h4</h4>
+    </>
   )
 }
 
