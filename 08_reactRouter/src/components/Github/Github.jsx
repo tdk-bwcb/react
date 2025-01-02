39b2c26c09457f1ccf2b9ee67332fc235ff5a6fb
tdk-bwcb <ygf2024@gmail.com>
2025-01-02 20:45:34 +0530

    React router crash course

---
+++ 08_reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+
+    const data = useLoaderData()
+
+    // const [data, setData] = useState({})
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/hiteshchoudhary')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data)
+    //         setData(data)
+    //     })
+    // }, [])
+
+  return (
+    <>
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}</div>
+        <img src={data.avatar_url} alt="Github avatar" className=' bg-gray-600 p-2 m-4 ' />
+    </>
+    
+  )
+}
+
+export default Github
+
+// more optimized way (faster)
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json()
+}
